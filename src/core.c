401920da3141aa35fbdba7dd33df8a6bfb20814e
flrl <flrl@users.noreply.github.com>
2012-08-19 21:23:39 +1000

    stubbed socket read processing

--- src/core.c
+++ src/core.c
@@ -19,18 +19,28 @@ void *_goat_core (void *arg) {
         pthread_mutex_unlock(&core_state.mutex);
 
         FD_ZERO(&readfds);
-        FD_SET(notify_fd, &readfds);
-
         FD_ZERO(&writefds);
 
-        // TODO loop over all the current connections and add their fds to the read/write sets
+        FD_SET(notify_fd, &readfds);
+        nfds = notify_fd;
 
-        // n.b. nfds = max(fds) + 1
-        nfds = notify_fd + 1;
-
-        if (select(nfds, &readfds, &writefds, NULL, NULL) > 0) {
-            // there are descriptors ready for action, go and do stuff with them
+        pthread_mutex_lock(&core_connection_list.mutex);
+        for (unsigned i = 0; i < core_connection_list.connection_pool_size; i++) {
+            if (core_connection_list.connection_pool[i] != NULL) {
+                FD_SET(core_connection_list.connection_pool[i].socket, &readfds);
+                FD_SET(core_connection_list.connection_pool[i].socket, &writefds);
+                if (core_connection_list.connection_pool[i].socket > nfds) {
+                    nfds = core_connection_list.connection_pool[i].socket;
+                }
+            }
         }
+        pthread_mutex_unlock(&core_connection_list.mutex);
+
+        if (select(nfds + 1, &readfds, NULL, NULL, NULL) > 0) {
+            // there are descriptors ready to read, go and process them
+        }
+
+        // TODO if we have pending outgoing messages, try to send them
 
         pthread_mutex_lock(&core_state.mutex);
     }
