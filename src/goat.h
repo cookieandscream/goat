03c54136ea14ba2ade0ca02193b8cdae38267722
flrl <flrl@users.noreply.github.com>
2012-10-08 20:20:34 +1100

    redefined public interface

--- src/goat.h
+++ src/goat.h
@@ -1,27 +1,45 @@
 #ifndef GOAT_H
 #define GOAT_H
 
-typedef int goat_handle;
-typedef void (*goat_callback)(goat_handle, const char *restrict, const char *restrict, const char **restrict);
+typedef struct s_goat_context goat_context;
+
+typedef void (*goat_callback)(
+    goat_context *,
+    int,
+    const char *restrict,
+    const char *restrict,
+    const char **restrict
+);
+
 typedef enum {
-    // TODO define some events
-    _dummy,
+    GOAT_EVENT_GENERIC,
 } goat_event;
 
-int goat_initialise(void);
-int goat_shutdown(void);
+goat_context *goat_context_new();
+int goat_context_delete(goat_context *);
 
-goat_handle goat_alloc(void);
-int goat_destroy(goat_handle);
+const char *goat_error(goat_context *);
 
-int goat_connect(goat_handle, const char *, int, int);
-int goat_disconnect(goat_handle);
-int goat_is_connected(goat_handle);
-int goat_get_connected_hostname(goat_handle, char **);
+int *goat_connection_new(goat_context *);
+int goat_connection_delete(goat_context *, int);
 
-int goat_send_message(goat_handle, const char *restrict, const char *restrict, const char **restrict);
+int goat_connect(goat_context *restrict, int, const char *restrict, int, int);
+int goat_disconnect(goat_context *restrict, int);
+int goat_is_connected(goat_context *restrict, int);
+int goat_get_hostname(goat_context *restrict, int, char **restrict);
 
-int goat_install_callback(goat_event, goat_callback);
-int goat_uninstall_callback(goat_event, goat_callback);
+int goat_send_message(
+    goat_context *restrict,
+    int,
+    const char *restrict,
+    const char *restrict,
+    const char **restrict
+);
+
+int goat_install_callback(goat_context *restrict, goat_event, goat_callback);
+int goat_uninstall_callback(goat_context *restrict, goat_event, goat_callback);
+
+int goat_tick(goat_context *restrict, struct timeval *restrict);
+int goat_dispatch_events(goat_context *restrict);
 
 #endif
