3076166c344d963d66da2975a8af616e932889d1
flrl <flrl@users.noreply.github.com>
2012-10-19 12:59:38 +1100

    removed obsolete stubs

--- src/goat.c
+++ src/goat.c
@@ -197,64 +197,6 @@ int goat_dispatch_events(goat_context_t *context) {
 }
 
 #if 0
-int core_thread_notify_fd;
-pthread_t core_thread;
-
-int goat_initialise(void) {
-    int fd[2];
-
-    if (0 != pipe(fd)) return -1;
-
-    core_thread_notify_fd = fd[1];
-    fcntl(core_thread_notify_fd, F_SETNOSIGPIPE, 1);
-
-    pthread_mutex_lock(&core_state.mutex);
-    core_state.running = 1;
-    pthread_create(&core_thread, NULL, &_goat_core, &fd[0]);
-    pthread_mutex_unlock(&core_state.mutex);
-
-    return 0;
-}
-
-int goat_shutdown(void) {
-    const char s[] = "shutdown\n"; // actual value is of no consequence
-
-    write(core_thread_notify_fd, s, sizeof s);
-    while (0 != pthread_mutex_trylock(&core_state.mutex)) {
-        static const struct timespec delay = { 0, 200 * 1000 * 1000 };  // 0.2s
-        nanosleep(&delay, NULL);
-        write(core_thread_notify_fd, s, sizeof s);
-    }
-    core_state.running = 0;
-    pthread_mutex_unlock(&core_state.mutex);
-    pthread_join(core_thread, NULL);
-
-    return 0;
-}
-
-goat_handle goat_alloc(void) {
-    return -1;
-}
-
-int goat_destroy(goat_handle handle) {
-    return -1;
-}
-
-int goat_connect(goat_handle handle, const char *hostname, int port, int ssl) {
-    return -1;
-}
-
-int goat_disconnect(goat_handle handle) {
-    return -1;
-}
-
-int goat_is_connected(goat_handle handle) {
-    return -1;
-}
-
-int goat_get_connected_hostname(goat_handle handle, char **retval) {
-    return -1;
-}
 
 int goat_send_message(
         goat_handle handle,
