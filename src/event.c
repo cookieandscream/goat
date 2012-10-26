e7b8141d31ed498be26a08083fcc613703acdccf
flrl <flrl@users.noreply.github.com>
2012-10-26 22:18:49 +1100

    use messages in event module

--- src/event.c
+++ src/event.c
@@ -4,6 +4,7 @@
 
 #include "context.h"
 #include "event.h"
+#include "message.h"
 
 static const goat_callback_t event_default_callbacks[GOAT_EVENT_LAST] = {
 
@@ -16,19 +17,16 @@ static const char *const event_commands[] = {
 };
 
 static int _event_commands_cmp(const void *a, const void *b);
-static int _event_parse_msg(const char *msg, char **prefix, char **command, char ***params);
-static goat_event_t _event_get_type(const char *prefix, const char *command, const char **params);
+static goat_event_t _event_get_type(const goat_message_t *message);
 
-int event_process(goat_context_t *context, int connection, const char *message) {
+int event_process(goat_context_t *context, int connection, const char *msg) {
     assert(context != NULL);
-    assert(message != NULL);
+    assert(msg != NULL);
 
-    char *prefix, *command, **params;
+    goat_message_t *message = message_new_from_string(msg, strlen(msg));
+    if (message == NULL)  return -1;
 
-    int ret = _event_parse_msg(message, &prefix, &command, &params);
-    if (ret != 0)  return ret;
-
-    goat_event_t event = _event_get_type(prefix, command, params);  // FIXME warning?
+    goat_event_t event = _event_get_type(message);
 
     // FIXME if there's a callback for it, call that
 
@@ -38,106 +36,12 @@ int event_process(goat_context_t *context, int connection, const char *message) 
 
     // FIXME or if there's a default generic callback, call that
 
+    message_delete(message);
     return -1; // FIXME
 }
 
-goat_event_t _event_get_type(const char *prefix, const char *command, const char **params) {
+goat_event_t _event_get_type(const goat_message_t *message) {
     return GOAT_EVENT_GENERIC; // FIXME
-}
-
-int _event_parse_msg(const char *msg, char **r_prefix, char **r_command, char ***r_params) {
-    char *prefix = NULL, *command = NULL, **params = NULL;
-    char *msg_copy, *remainder, *token, *end;
-    int ret = -1;
-
-    remainder = msg_copy = strdup(msg);
-    if (remainder == NULL)  return GOAT_E_NOMEM;
-
-    // chomp crlf
-    end = strchr(remainder, '\0');
-    if (*(end - 1) == '\x0a')  *(--end) = '\0';
-    if (*(end - 1) == '\x0d')  *(--end) = '\0';
-
-    // [ ':' prefix SPACE ]
-    if (*remainder == ':') {
-        ++ remainder;
-        token = strsep(&remainder, " ");
-        if (*token == '\0') {
-            ret = GOAT_E_INVMSG;
-            goto cleanup;
-        }
-        prefix = strdup(token);
-        if (prefix == NULL) {
-            ret = GOAT_E_NOMEM;
-            goto cleanup;
-        }
-    }
-
-    // command
-    if ((token = strsep(&remainder, " "))) {
-        if (*token == '\0') {
-            ret = GOAT_E_INVMSG;
-            goto cleanup;
-        }
-        command = strdup(token);
-        if (command == NULL) {
-            ret = GOAT_E_NOMEM;
-            goto cleanup;
-        }
-    }
-    else {
-        ret = GOAT_E_INVMSG;
-        goto cleanup;
-    }
-
-    // *14( SPACE middle ) [ SPACE ":" trailing ]
-    // 14( SPACE middle ) [ SPACE [ ":" ] trailing ]
-    if (remainder) {
-        params = calloc(16, sizeof(char *));
-        if (params == NULL) {
-            ret = GOAT_E_NOMEM;
-            goto cleanup;
-        }
-
-        unsigned i = 0;
-        while (i < 14 && remainder != NULL) {
-            if (*remainder == ':')  break;
-            token = strsep(&remainder, " ");
-            params[i] = strdup(token);
-            if (params[i] == NULL) {
-                ret = GOAT_E_NOMEM;
-                goto cleanup;
-            }
-            ++ i;
-        }
-
-        if (remainder) {
-            if (*remainder == ':')  ++remainder;
-            params[i] = strdup(remainder);
-            if (params[i] == NULL) {
-                ret = GOAT_E_NOMEM;
-                goto cleanup;
-            }
-        }
-    }
-
-    free(msg_copy);
-    *r_prefix = prefix;
-    *r_command = command;
-    *r_params = params;
-    return 0;
-
-cleanup:
-    if (params) {
-        char **p = params;
-        while (*p)  free(*(p++));
-        free(params);
-    }
-    if (command)  free(command);
-    if (prefix)  free(prefix);
-
-    free(msg_copy);
-    return ret;
 }
 
 int _event_commands_cmp(const void *a, const void *b) {
