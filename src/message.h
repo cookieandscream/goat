8532d9caa3d91952d092dadeaa69a832a0061091
flrl <flrl@users.noreply.github.com>
2015-03-25 13:03:44 +1100

    more prototyping

--- src/message.h
+++ src/message.h
@@ -23,6 +23,23 @@ const char *message_static_command(const char *command);
 
 ///
 
+// FIXME these structs will need to be private
+struct s_kv_list_node {
+    char *key;
+    char *value;
+    struct s_kv_list_node *prev;
+    struct s_kv_list_node *next;
+};
+
+struct s_goat_message {
+    struct s_kv_list_node *m_tags;
+    char *m_prefix;
+    char *m_command;
+    char *m_params[16];
+};
+
+typedef struct s_goat_message goat_message_t;
+
 goat_message_t *goat_message_new(const char *prefix, const char *command, const char **params);
 goat_message_t *goat_message_new_from_string(const char *str, size_t len);
 goat_message_t *goat_message_clone(const goat_message_t *orig);
@@ -39,6 +56,7 @@ size_t goat_message_get_nparams(const goat_message_t *message);
 int goat_message_get_param(const goat_message_t *message, int index, char *param, size_t *size);
 
 int goat_message_set_tag(goat_message_t *message, const char *key, const char *value);
+size_t goat_message_has_tags(const goat_message_t *message);
 int goat_message_has_tag(const goat_message_t *message, const char *key);
 int goat_message_get_tag(const goat_message_t *message, const char *key, char *value, size_t *size);
 int goat_message_unset_tag(goat_message_t *message, const char *key);
