02bbf70a77dd7d14622d1b88eb14365cc0045e0b
flrl <flrl@users.noreply.github.com>
2015-03-27 19:38:25 +1100

    move giant command array into own module

---
+++ src/commands.h
@@ -0,0 +1,6 @@
+#ifndef GOAT_COMMANDS_H
+#define GOAT_COMMANDS_H
+
+extern const char *const message_commands[];
+
+#endif
--- src/message.h
+++ src/message.h
@@ -1,6 +1,5 @@
 #ifndef GOAT_MESSAGE_H
 #define GOAT_MESSAGE_H
 
-extern const char *const message_commands[];
 
 #endif
