0515de7925700035c271a5ad0372fffb4b30925a
flrl <flrl@users.noreply.github.com>
2015-03-29 13:58:54 +1100

    stub a test suite

---
+++ cunit/message/constructor.c
@@ -0,0 +1,21 @@
+#include "CUnit/CUnit.h"
+#include "CUnit/Basic.h"
+
+#include "src/message.h"
+
+void test_goat_message_new(void) {
+    const char *prefix = "prefix";
+    const char *command = "command";
+
+    goat_message_t *message = goat_message_new(prefix, command, NULL);
+
+    CU_ASSERT(message != NULL);
+    CU_ASSERT_STRING_EQUAL(message->m_prefix, prefix);
+    CU_ASSERT_STRING_EQUAL(message->m_command, command);
+
+    // FIXME assert that m_prefix points into m_bytes
+    // FIXME assert that m_command points into m_bytes or the big commands array
+    // FIXME functionality of params
+
+    goat_message_delete(message);
+}
---
+++ cunit/message/suite.c
@@ -0,0 +1,10 @@
+#include "CUnit/CUnit.h"
+#include "CUnit/Basic.h"
+
+int message_suite_init(void) {
+    return 0;
+}
+
+int message_suite_cleanup(void) {
+    return 0;
+}
