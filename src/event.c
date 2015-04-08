04882f52b832834717af98a3b4a7eb86bc996d6c
flrl <flrl@users.noreply.github.com>
2015-04-08 14:46:20 +1000

    events: primary and secondary types

--- src/event.c
+++ src/event.c
@@ -4,27 +4,34 @@
 
 #include "context.h"
 #include "event.h"
+#include "irc.h"
 #include "message.h"
 
 static const goat_callback_t event_default_callbacks[GOAT_EVENT_LAST] = {
 
 };
 
-static goat_event_t _event_get_type(const goat_message_t *message);
+static const goat_event_pair_t *_event_get_type(const goat_message_t *message);
 
 int event_process(goat_context_t *context, int connection, const goat_message_t *message) {
     assert(context != NULL);
     assert(message != NULL);
 
-    goat_event_t event = _event_get_type(message);
+    const goat_event_pair_t *e = _event_get_type(message);
 
     goat_callback_t callback = NULL;
 
-    if (NULL != context->m_callbacks[event]) {
-        callback = context->m_callbacks[event];
+    if (NULL != context->m_callbacks[e->primary]) {
+        callback = context->m_callbacks[e->primary];
     }
-    else if (NULL != event_default_callbacks[event]) {
-        callback = event_default_callbacks[event];
+    else if (NULL != event_default_callbacks[e->primary]) {
+        callback = event_default_callbacks[e->primary];
+    }
+    else if (NULL != context->m_callbacks[e->secondary]) {
+        callback = context->m_callbacks[e->secondary];
+    }
+    else if (NULL != event_default_callbacks[e->secondary]) {
+        callback = event_default_callbacks[e->secondary];
     }
     else if (NULL != context->m_callbacks[GOAT_EVENT_GENERIC]) {
         callback = context->m_callbacks[GOAT_EVENT_GENERIC];
@@ -41,6 +48,6 @@ int event_process(goat_context_t *context, int connection, const goat_message_t 
     return 0;
 }
 
-goat_event_t _event_get_type(const goat_message_t *message) {
-    return GOAT_EVENT_GENERIC; // FIXME
+const goat_event_pair_t *_event_get_type(const goat_message_t *message) {
+    return &irc_events[0]; // FIXME
 }
--- src/irc.h
+++ src/irc.h
@@ -1,6 +1,13 @@
 #ifndef GOAT_IRC_H
 #define GOAT_IRC_H
 
+typedef struct {
+    goat_event_t primary;
+    goat_event_t secondary;
+} goat_event_pair_t;
+
 extern const char *const irc_strings[];
+
+extern const goat_event_pair_t irc_events[];
 
 #endif
