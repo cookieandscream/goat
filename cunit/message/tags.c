90ebd66cac07a956103fce28fdcd2a1e78d8dd59
flrl <flrl@users.noreply.github.com>
2015-04-12 15:33:38 +1000

    added some more tags tests

--- cunit/message/tags.c
+++ cunit/message/tags.c
@@ -1,12 +1,15 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 
+#include <assert.h>
+#include <stdlib.h>
+
 #include "src/goat.h"
 #include "src/message.h"
 
 goat_message_t *msg;
 
-static void _clear_message_tags(goat_message_t *message);
+static void _set_tags(goat_message_t *message, const char *raw_tags);
 
 int message_tags_suite_init(void) {
     msg = goat_message_new(NULL, "PRIVMSG", NULL);
@@ -25,45 +28,74 @@ void test_goat__message__has__tags___without_message(void) {
 }
 
 void test_goat__message__has__tags___without_tags(void) {
-    _clear_message_tags(msg);
+    _set_tags(msg, NULL);
     CU_ASSERT_EQUAL(goat_message_has_tags(msg), 0);
 }
 
 void test_goat__message__has__tags___with_tag(void) {
-    _clear_message_tags(msg);
-
-    int ret = goat_message_set_tag(msg, "somekey", "somevalue");
-    CU_ASSERT_EQUAL_FATAL(ret, 0);
+    _set_tags(msg, "somekey");
 
     CU_ASSERT_EQUAL(goat_message_has_tags(msg), 1);
-    _clear_message_tags(msg);
+
+    _set_tags(msg, "somekey;");
+
+    CU_ASSERT_EQUAL(goat_message_has_tags(msg), 1);
+
+    _set_tags(msg, "somekey=somevalue");
+
+    CU_ASSERT_EQUAL(goat_message_has_tags(msg), 1);
+
+    _set_tags(msg, "somekey=somevalue;");
+
+    CU_ASSERT_EQUAL(goat_message_has_tags(msg), 1);
 }
 
 void test_goat__message__has__tags___with_tags(void) {
-    _clear_message_tags(msg);
+    _set_tags(msg, "key1=val1;key2;key3=val3;key4;key5;");
 
-    const size_t n_tags = 5;
-    for (size_t i = 0; i < n_tags; i++) {
-        char key[5], val[5];
-        snprintf(key, sizeof(key), "%s%zu", "key", i);
-        snprintf(val, sizeof(val), "%s%zu", "val", i);
-        int ret = goat_message_set_tag(msg, key, val);
-        CU_ASSERT_EQUAL_FATAL(ret, 0);
-    }
-
-    CU_ASSERT_EQUAL(goat_message_has_tags(msg), n_tags);
-    _clear_message_tags(msg);
+    CU_ASSERT_EQUAL(goat_message_has_tags(msg), 5);
 }
 
-//size_t goat_message_has_tags(const goat_message_t *message);
+void test_goat__message__has__tags___with_special_value(void) {
+    _set_tags(msg, "plain=text;encoded=text\\:with\\:separators;one\\smore");
+
+    CU_ASSERT_EQUAL(goat_message_has_tags(msg), 3);
+}
+
+void test_goat__message__has__tag___when_it_does_not(void) {
+    _set_tags(msg, "key1=val1;key2=val2;key3=val3");
+
+    CU_ASSERT_FALSE(goat_message_has_tag(msg, "key4"));
+}
+
+void test_goat__message__has__tag___when_it_does(void) {
+    _set_tags(msg, "key1=val1;key2=val2;key3;key4;key5=val5");
+
+    CU_ASSERT_TRUE(goat_message_has_tag(msg, "key1"));
+    CU_ASSERT_TRUE(goat_message_has_tag(msg, "key2"));
+    CU_ASSERT_TRUE(goat_message_has_tag(msg, "key3"));
+    CU_ASSERT_TRUE(goat_message_has_tag(msg, "key4"));
+    CU_ASSERT_TRUE(goat_message_has_tag(msg, "key5"));
+}
+
 //int goat_message_has_tag(const goat_message_t *message, const char *key);
 //int goat_message_get_tag_value(const goat_message_t *message, const char *key, char *value, size_t *size);
 //int goat_message_set_tag(goat_message_t *message, const char *key, const char *value);
 //int goat_message_unset_tag(goat_message_t *message, const char *key);
 
-static void _clear_message_tags(goat_message_t *message) {
-    if (message && message->m_tags) {
-        memset(message->m_tags, 0, sizeof(*message->m_tags));
+static void _set_tags(goat_message_t *message, const char *raw_tags) {
+    assert(NULL != message);
+
+    if (NULL == message->m_tags) {
+        message->m_tags = calloc(1, sizeof(*message->m_tags));
+        CU_ASSERT_PTR_NOT_NULL_FATAL(message->m_tags);
+    }
+
+    memset(message->m_tags, 0, sizeof(*message->m_tags));
+
+    if (raw_tags) {
+        message->m_tags->m_len = strlen(raw_tags);
+        strcpy(message->m_tags->m_bytes, raw_tags);
     }
 }
 
