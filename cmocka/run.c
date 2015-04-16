3da25663fc74079d939d2d7514a106cf2a536a80
flrl <flrl@users.noreply.github.com>
2015-04-16 12:59:21 +1000

    output test results in TAP format

--- cmocka/run.c
+++ cmocka/run.c
@@ -5,6 +5,8 @@ extern const goat_test_group_t *test_groups[];
 int main (int argc __attribute__((unused)), char **argv __attribute__((unused))) {
     int errors = 0;
 
+    cmocka_set_message_output(CM_OUTPUT_TAP);
+
     for (size_t g = 0; test_groups[g] != NULL; g++) {
         const goat_test_group_t *group = test_groups[g];
 
