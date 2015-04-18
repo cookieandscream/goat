0ab7f18305f7cc315a29afb6c62810abae3231b7
flrl <flrl@users.noreply.github.com>
2015-04-18 19:36:26 +1000

    accessor tests: use state

--- cmocka/message/accessor.c
+++ cmocka/message/accessor.c
@@ -1,73 +1,96 @@
+#include <stdlib.h>
+
 #include "cmocka/run.h"
 
 #include "src/goat.h"
 #include "src/message.h"
 
-goat_message_t *msg;
-goat_message_t *msg_w_prefix;
-goat_message_t *msg_w_params;
-goat_message_t *msg_w_prefix_params;
+struct objs {
+    goat_message_t *msg;
+    goat_message_t *msg_w_prefix;
+    goat_message_t *msg_w_params;
+    goat_message_t *msg_w_prefix_params;
+};
 
 int message_accessor_group_setup(void **state) {
     const char *prefix = "prefix";
     const char *command = "command";
     const char *params[] = { "param1", "param2", "param3", NULL };
 
-    msg = goat_message_new(NULL, command, NULL);
-    msg_w_prefix = goat_message_new(prefix, command, NULL);
-    msg_w_params = goat_message_new(NULL, command, params);
-    msg_w_prefix_params = goat_message_new(prefix, command, params);
+    struct objs *objs = calloc(1, sizeof(*objs));
+    if (NULL == objs) return -1;
 
-    if (msg && msg_w_prefix && msg_w_params && msg_w_prefix_params) return 0;
+    objs->msg = goat_message_new(NULL, command, NULL);
+    if (NULL == objs->msg) goto cleanup;
 
-    if (msg) goat_message_delete(msg);
-    if (msg_w_prefix) goat_message_delete(msg_w_prefix);
-    if (msg_w_params) goat_message_delete(msg_w_params);
-    if (msg_w_prefix_params) goat_message_delete(msg_w_prefix_params);
+    objs->msg_w_prefix = goat_message_new(prefix, command, NULL);
+    if (NULL == objs->msg) goto cleanup;
+
+    objs->msg_w_params = goat_message_new(NULL, command, params);
+    if (NULL == objs->msg) goto cleanup;
+
+    objs->msg_w_prefix_params = goat_message_new(prefix, command, params);
+    if (NULL == objs->msg) goto cleanup;
+
+    *state = objs;
+    return 0;
+
+cleanup:
+    if (objs->msg) goat_message_delete(objs->msg);
+    if (objs->msg_w_prefix) goat_message_delete(objs->msg_w_prefix);
+    if (objs->msg_w_params) goat_message_delete(objs->msg_w_params);
+    if (objs->msg_w_prefix_params) goat_message_delete(objs->msg_w_prefix_params);
+    free(objs);
     return -1;
 }
 
 int message_accessor_group_teardown(void **state) {
-    goat_message_delete(msg);
-    goat_message_delete(msg_w_prefix);
-    goat_message_delete(msg_w_params);
-    goat_message_delete(msg_w_prefix_params);
+    struct objs *objs = * (struct objs **) state;
+    *state = NULL;
+    goat_message_delete(objs->msg);
+    goat_message_delete(objs->msg_w_prefix);
+    goat_message_delete(objs->msg_w_params);
+    goat_message_delete(objs->msg_w_prefix_params);
+    free(objs);
     return 0;
 }
 
-void test_goat__message__get__prefix___without_message(void **state) {
+void test_goat__message__get__prefix___without_message(void **state UNUSED) {
     const char *str = goat_message_get_prefix(NULL);
 
     assert_null(str);
 }
 
 void test_goat__message__get__prefix___without_prefix(void **state) {
-    const char *str = goat_message_get_prefix(msg);
+    struct objs *objs = * (struct objs **) state;
+    const char *str = goat_message_get_prefix(objs->msg);
 
     assert_null(str);
 }
 
 void test_goat__message__get__prefix___with_prefix(void **state) {
-    const char *str = goat_message_get_prefix(msg_w_prefix);
+    struct objs *objs = * (struct objs **) state;
+    const char *str = goat_message_get_prefix(objs->msg_w_prefix);
 
     assert_non_null(str);
     assert_string_equal(str, "prefix");
 }
 
-void test_goat__message__get__command__string___without_message(void **state) {
+void test_goat__message__get__command__string___without_message(void **state UNUSED) {
     const char *str = goat_message_get_command_string(NULL);
 
     assert_null(str);
 }
 
 void test_goat__message__get__command__string___with_unrecognised_command(void **state) {
-    const char *str = goat_message_get_command_string(msg);
+    struct objs *objs = * (struct objs **) state;
+    const char *str = goat_message_get_command_string(objs->msg);
 
     assert_non_null(str);
     assert_string_equal(str, "command");
 }
 
-void test_goat__message__get__command__string___with_recognised_command(void **state) {
+void test_goat__message__get__command__string___with_recognised_command(void **state UNUSED) {
     const char *command = "PRIVMSG";
 
     goat_message_t *message = goat_message_new(NULL, command, NULL);
@@ -82,30 +105,33 @@ void test_goat__message__get__command__string___with_recognised_command(void **s
     goat_message_delete(message);
 }
 
-void test_goat__message__get__param___without_message(void **state) {
+void test_goat__message__get__param___without_message(void **state UNUSED) {
     const char *str = goat_message_get_param(NULL, 0);
 
     assert_null(str);
 }
 
 void test_goat__message__get__param___without_params(void **state) {
-    const char *str = goat_message_get_param(msg, 0);
+    struct objs *objs = * (struct objs **) state;
+    const char *str = goat_message_get_param(objs->msg, 0);
 
     assert_null(str);
 }
 
 void test_goat__message__get__param___with_excessive_size(void **state) {
-    const char *str = goat_message_get_param(msg_w_params, 16);
+    struct objs *objs = * (struct objs **) state;
+    const char *str = goat_message_get_param(objs->msg_w_params, 16);
 
     assert_null(str);
 }
 
 void test_goat__message__get__param___with_params(void **state) {
+    struct objs *objs = * (struct objs **) state;
     const char *expect_params[] = { "param1", "param2", "param3", NULL };
     size_t expect_nparams = 3;
 
     for (size_t i = 0; i < 16; i++) {
-        const char *str = goat_message_get_param(msg_w_params, i);
+        const char *str = goat_message_get_param(objs->msg_w_params, i);
 
         if (i < expect_nparams) {
             assert_non_null(str);
@@ -117,40 +143,43 @@ void test_goat__message__get__param___with_params(void **state) {
     }
 }
 
-void test_goat__message__get__nparams___without_message(void **state) {
+void test_goat__message__get__nparams___without_message(void **state UNUSED) {
     size_t n = goat_message_get_nparams(NULL);
 
     assert_int_equal(n, 0);
 }
 
 void test_goat__message__get__nparams___with_params(void **state) {
-    size_t n = goat_message_get_nparams(msg_w_params);
+    struct objs *objs = * (struct objs **) state;
+    size_t n = goat_message_get_nparams(objs->msg_w_params);
 
     assert_int_equal(n, 3);
 }
 
-void test_goat__message__get__command___without_message(void **state) {
+void test_goat__message__get__command___without_message(void **state UNUSED) {
     int r = goat_message_get_command(NULL, NULL);
 
     assert_int_equal(r, -1);
 }
 
 void test_goat__message__get__command___without_command(void **state) {
-    int r = goat_message_get_command(msg, NULL);
+    struct objs *objs = * (struct objs **) state;
+    int r = goat_message_get_command(objs->msg, NULL);
 
     assert_int_equal(r, -1);
 }
 
 void test_goat__message__get__command___with_unrecognised_command(void **state) {
+    struct objs *objs = * (struct objs **) state;
     goat_command_t cmd = GOAT_IRC_LAST;
 
-    int r = goat_message_get_command(msg, &cmd);
+    int r = goat_message_get_command(objs->msg, &cmd);
 
     assert_int_equal(r, -1);
     assert_int_equal(cmd, GOAT_IRC_LAST);
 }
 
-void test_goat__message__get__command___with_recognised_command(void **state) {
+void test_goat__message__get__command___with_recognised_command(void **state UNUSED) {
     goat_message_t *message = goat_message_new(NULL, "PRIVMSG", NULL);
     assert_non_null(message);
 
