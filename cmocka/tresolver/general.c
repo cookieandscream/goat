5e474277f15e0e9942d9b48c03f0c13c17a31018
flrl <flrl@users.noreply.github.com>
2015-04-25 13:06:33 +1000

    tresolver tests: protect global mock state

--- cmocka/tresolver/general.c
+++ cmocka/tresolver/general.c
@@ -29,6 +29,7 @@ struct resolver_state {
     char *servname;
 };
 
+// global state for mocked getaddrinfo
 struct getaddrinfo_mock {
     void **state;
     const char *expect_hostname;
@@ -36,7 +37,42 @@ struct getaddrinfo_mock {
     struct timespec delay;
     struct addrinfo **out_res;
     int out_return;
-} getaddrinfo_mock;
+} g_getaddrinfo_mock;
+
+pthread_mutex_t g_getaddrinfo_mock_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static int mock_init(void **state, const char *expect_hostname, const char *expect_servname,
+    struct timespec delay, struct addrinfo *out_res, int out_return)
+{
+    struct addrinfo **tmp = calloc(1, sizeof(*tmp));
+    if (NULL == tmp) return errno;
+    *tmp = out_res;
+
+    int r = pthread_mutex_lock(&g_getaddrinfo_mock_mutex);
+    if (r) return r;
+
+    g_getaddrinfo_mock.state = state;
+    g_getaddrinfo_mock.expect_hostname = expect_hostname;
+    g_getaddrinfo_mock.expect_servname = expect_servname;
+    g_getaddrinfo_mock.delay = delay;
+    g_getaddrinfo_mock.out_return = out_return;
+
+    g_getaddrinfo_mock.out_res = tmp;
+
+    r = pthread_mutex_unlock(&g_getaddrinfo_mock_mutex);
+    return r;
+}
+
+static int mock_destroy(int freeres) {
+    int r = pthread_mutex_lock(&g_getaddrinfo_mock_mutex);
+    if (r) return r;
+
+    if (freeres) free(g_getaddrinfo_mock.out_res);
+    memset(&g_getaddrinfo_mock, 0, sizeof(g_getaddrinfo_mock));
+
+    r = pthread_mutex_unlock(&g_getaddrinfo_mock_mutex);
+    return r;
+}
 
 int __wrap_getaddrinfo(const char *h, const char *s,
     const struct addrinfo *i, struct addrinfo **res)
@@ -44,7 +80,13 @@ int __wrap_getaddrinfo(const char *h, const char *s,
     ARG_UNUSED(i);
 
     struct getaddrinfo_mock mock;
-    memcpy(&mock, &getaddrinfo_mock, sizeof(mock));
+
+    int r = pthread_mutex_lock(&g_getaddrinfo_mock_mutex);
+    if (r) return r;
+
+    memcpy(&mock, &g_getaddrinfo_mock, sizeof(mock));
+
+    pthread_mutex_unlock(&g_getaddrinfo_mock_mutex);
 
     assert_string_equal(h, mock.expect_hostname);
     assert_string_equal(s, mock.expect_servname);
@@ -61,18 +103,9 @@ int __wrap_getaddrinfo(const char *h, const char *s,
 /* ====================================================================== */
 
 int setup_resolver__getaddrinfo___with_successful_name_resolution(void **state) {
-    getaddrinfo_mock.state = state;
-    getaddrinfo_mock.expect_hostname = "irc.example.com";
-    getaddrinfo_mock.expect_servname = "6667";
-    getaddrinfo_mock.delay.tv_sec = 0;
-    getaddrinfo_mock.delay.tv_nsec = HALF_SECOND;
-    getaddrinfo_mock.out_return = 0;
+    struct timespec delay = { 0, HALF_SECOND };
 
-    getaddrinfo_mock.out_res = calloc(1, sizeof(*getaddrinfo_mock.out_res));
-    if (NULL == getaddrinfo_mock.out_res) return -1;
-
-    *getaddrinfo_mock.out_res = (struct addrinfo *) "PACIFIER";
-    return 0;
+    return mock_init(state, "irc.example.com", "6667", delay, (struct addrinfo *) "PACIFIER", 0);
 }
 
 void test_resolver__getaddrinfo___with_successful_name_resolution(void **state) {
@@ -85,8 +118,8 @@ void test_resolver__getaddrinfo___with_successful_name_resolution(void **state) 
     assert_non_null(resolver_state);
     assert_null(ai);
 
-    nanosleep(&getaddrinfo_mock.delay, NULL);
-    nanosleep(&getaddrinfo_mock.delay, NULL);
+    struct timespec wait = { 1, 0 };
+    nanosleep(&wait, NULL);
 
     r = resolver_getaddrinfo(&resolver_state, "irc.example.com", "6667", &ai);
 
@@ -98,26 +131,16 @@ void test_resolver__getaddrinfo___with_successful_name_resolution(void **state) 
 
 int teardown_resolver__getaddrinfo___with_successful_name_resolution(void **state) {
     ARG_UNUSED(state);
-    free(getaddrinfo_mock.out_res);
-    memset(&getaddrinfo_mock, 0, sizeof(getaddrinfo_mock));
-    return 0;
+
+    return mock_destroy(1);
 }
 
 /* ====================================================================== */
 
 int setup_resolver__getaddrinfo___with_unsuccessful_name_resolution(void **state) {
-    getaddrinfo_mock.state = state;
-    getaddrinfo_mock.expect_hostname = "irc.example.com";
-    getaddrinfo_mock.expect_servname = "6667";
-    getaddrinfo_mock.delay.tv_sec = 0;
-    getaddrinfo_mock.delay.tv_nsec = HALF_SECOND;
-    getaddrinfo_mock.out_return = EAI_FAIL;
+    struct timespec delay = { 0, HALF_SECOND };
 
-    getaddrinfo_mock.out_res = calloc(1, sizeof(*getaddrinfo_mock.out_res));
-    if (NULL == getaddrinfo_mock.out_res) return -1;
-
-    *getaddrinfo_mock.out_res = NULL;
-    return 0;
+    return mock_init(state, "irc.example.com", "6667", delay, NULL, EAI_FAIL);
 }
 
 void test_resolver__getaddrinfo___with_unsuccessful_name_resolution(void **state) {
@@ -130,8 +153,8 @@ void test_resolver__getaddrinfo___with_unsuccessful_name_resolution(void **state
     assert_non_null(resolver_state);
     assert_null(ai);
 
-    nanosleep(&getaddrinfo_mock.delay, NULL);
-    nanosleep(&getaddrinfo_mock.delay, NULL);
+    struct timespec wait = { 1, 0 };
+    nanosleep(&wait, NULL);
 
     r = resolver_getaddrinfo(&resolver_state, "irc.example.com", "6667", &ai);
 
@@ -142,26 +165,16 @@ void test_resolver__getaddrinfo___with_unsuccessful_name_resolution(void **state
 
 int teardown_resolver__getaddrinfo___with_unsuccessful_name_resolution(void **state) {
     ARG_UNUSED(state);
-    free(getaddrinfo_mock.out_res);
-    memset(&getaddrinfo_mock, 0, sizeof(getaddrinfo_mock));
-    return 0;
+
+    return mock_destroy(1);
 }
 
 /* ====================================================================== */
 
 int setup_resolver__getaddrinfo___with_cancelled_request(void **state) {
-    getaddrinfo_mock.state = state;
-    getaddrinfo_mock.expect_hostname = "irc.example.com";
-    getaddrinfo_mock.expect_servname = "6667";
-    getaddrinfo_mock.delay.tv_sec = 2;
-    getaddrinfo_mock.delay.tv_nsec = 0;
-    getaddrinfo_mock.out_return = EAI_FAIL;
+    struct timespec delay = { 2, 0 };
 
-    getaddrinfo_mock.out_res = calloc(1, sizeof(*getaddrinfo_mock.out_res));
-    if (NULL == getaddrinfo_mock.out_res) return -1;
-
-    *getaddrinfo_mock.out_res = NULL;
-    return 0;
+    return mock_init(state, "irc.example.com", "6667", delay, NULL, EAI_FAIL);
 }
 
 void test_resolver__getaddrinfo___with_cancelled_request(void **state) {
@@ -191,26 +204,16 @@ void test_resolver__getaddrinfo___with_cancelled_request(void **state) {
 
 int teardown_resolver__getaddrinfo___with_cancelled_request(void **state) {
     ARG_UNUSED(state);
-    // don't free getaddrinfo_mock.out_res, the cancelled thread will do it
-    memset(&getaddrinfo_mock, 0, sizeof(getaddrinfo_mock));
-    return 0;
+
+    return mock_destroy(0);
 }
 
 /* ====================================================================== */
 
 int setup_resolver__getaddrinfo___with_slow_lookup(void **state) {
-    getaddrinfo_mock.state = state;
-    getaddrinfo_mock.expect_hostname = "irc.example.com";
-    getaddrinfo_mock.expect_servname = "6667";
-    getaddrinfo_mock.delay.tv_sec = 2;
-    getaddrinfo_mock.delay.tv_nsec = HALF_SECOND;
-    getaddrinfo_mock.out_return = 0;
+    struct timespec delay = { 2, HALF_SECOND };
 
-    getaddrinfo_mock.out_res = calloc(1, sizeof(*getaddrinfo_mock.out_res));
-    if (NULL == getaddrinfo_mock.out_res) return -1;
-
-    *getaddrinfo_mock.out_res = (struct addrinfo *) "PACIFIER";
-    return 0;
+    return mock_init(state, "irc.example.com", "6667", delay, (struct addrinfo *) "PACIFIER", 0);
 }
 
 void test_resolver__getaddrinfo___with_slow_lookup(void **state) {
@@ -235,8 +238,8 @@ void test_resolver__getaddrinfo___with_slow_lookup(void **state) {
     assert_non_null(resolver_state);
     assert_null(ai);
 
-    nanosleep(&getaddrinfo_mock.delay, NULL);
-    nanosleep(&getaddrinfo_mock.delay, NULL);
+    struct timespec wait = { 5, 0 };
+    nanosleep(&wait, NULL);
 
     r = resolver_getaddrinfo(&resolver_state, "irc.example.com", "6667", &ai);
 
@@ -248,7 +251,6 @@ void test_resolver__getaddrinfo___with_slow_lookup(void **state) {
 
 int teardown_resolver__getaddrinfo___with_slow_lookup(void **state) {
     ARG_UNUSED(state);
-    free(getaddrinfo_mock.out_res);
-    memset(&getaddrinfo_mock, 0, sizeof(getaddrinfo_mock));
-    return 0;
+
+    return mock_destroy(1);
 }
