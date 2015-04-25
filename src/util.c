ff5cbeb99521e13ee59d6cd0cba7c65d87986404
flrl <flrl@users.noreply.github.com>
2015-04-25 10:14:54 +1000

    use inline, this is c99

--- src/util.c
+++ src/util.c
@@ -1,19 +1,7 @@
 #include "util.h"
 
-int strn_has_crlf(const char *str, size_t len) {
-    char buf[len + 1];
+extern inline int str_has_crlf(const char *str);
+extern inline int str_has_sp(const char *str);
 
-    strncpy(buf, str, len);
-    buf[len] = '\0';
-
-    return str_has_crlf(buf);
-}
-
-int strn_has_sp(const char *str, size_t len) {
-    char buf[len + 1];
-
-    strncpy(buf, str, len);
-    buf[len] = '\0';
-
-    return str_has_sp(buf);
-}
+extern inline int strn_has_crlf(const char *str, size_t len);
+extern inline int strn_has_sp(const char *str, size_t len);
--- src/util.h
+++ src/util.h
@@ -3,10 +3,30 @@
 
 #include <string.h>
 
-#define str_has_crlf(s)     (strcspn((s), "\x0d\x0a") < strlen((s)))
-#define str_has_sp(s)       (NULL != strchr((s), ' '))
+inline int str_has_crlf(const char *str) {
+    return strcspn(str, "\x0d\x0a") < strlen(str);
+}
 
-int strn_has_crlf(const char *str, size_t len);
-int strn_has_sp(const char *str, size_t len);
+inline int str_has_sp(const char *str) {
+    return NULL != strchr(str, ' ');
+}
+
+inline int strn_has_crlf(const char *str, size_t len) {
+    char buf[len + 1];
+
+    strncpy(buf, str, len);
+    buf[len] = '\0';
+
+    return str_has_crlf(buf);
+}
+
+inline int strn_has_sp(const char *str, size_t len) {
+    char buf[len + 1];
+
+    strncpy(buf, str, len);
+    buf[len] = '\0';
+
+    return str_has_sp(buf);
+}
 
 #endif
